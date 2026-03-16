50a10a4ed7e511d7993c96b3a8137f253c039043
reberapower1 <dianinhalindamaravilhosa@gmail.com>
2026-03-16 12:15:47 +0000

    mod operation

---
+++ Code/calculator/our_mod/test_our_mod.py
@@ -0,0 +1,15 @@
+import pytest
+from our_mod import our_mod
+
+
+def test_trivial_mod():
+    assert our_mod(17) == 17, "Should be 17"
+
+def test_for_negatives():
+    assert our_mod(-3) == 3, "Should be 3"
+
+@pytest.mark.parametrize("a,result",[(-3,3)])
+
+def test_multiples(a, result):
+   assert result == our_mod(a)
+
