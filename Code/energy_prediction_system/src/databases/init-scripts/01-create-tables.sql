a85ea8e28b6d3f966dc0f1a1c2acf993f7ae18ed
franciscamateusPt05 <mateusfrancisca2005@gmail.com>
2026-04-16 12:19:48 +0100

    Base de dados terminadas

--- Code/energy_prediction_system/src/databases/init-scripts/01-create-tables.sql
+++ Code/energy_prediction_system/src/databases/init-scripts/01-create-tables.sql
@@ -1,38 +1,69 @@
--- Criar extensões úteis (opcional)
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
+DROP TABLE IF EXISTS predictions_hourly CASCADE;
+DROP TABLE IF EXISTS predictions_daily CASCADE;
+DROP TABLE IF EXISTS request CASCADE;
+DROP TABLE IF EXISTS admin CASCADE;
+DROP TABLE IF EXISTS client CASCADE;
+DROP TABLE IF EXISTS model CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 
--- 1. Tabela Principal de Utilizadores
 CREATE TABLE users (
-    id BIGSERIAL PRIMARY KEY,
-    email TEXT NOT NULL UNIQUE,
-    password VARCHAR(512) NOT NULL
+    id                  BIGSERIAL,
+    email               TEXT NOT NULL UNIQUE,
+    password            VARCHAR(512) NOT NULL,
+    username            VARCHAR(512) NOT NULL UNIQUE,
+    account_regist_date TIMESTAMP NOT NULL,
+    failed_login_att    INTEGER NOT NULL DEFAULT 0,
+    acc_locked_until    TIMESTAMP,
+    last_failed_att     TIMESTAMP,
+    PRIMARY KEY(id)
 );
 
--- 2. Perfis (Herança de Users)
+CREATE TABLE model (
+    model_name_id              BIGSERIAL,
+    model_type                 VARCHAR(512) NOT NULL,
+    model_creation_date        TIMESTAMP NOT NULL,
+    model_server_relative_path VARCHAR(512) NOT NULL,
+    rmse                       DOUBLE PRECISION NOT NULL,
+    mae                        DOUBLE PRECISION NOT NULL,
+    r2                         DOUBLE PRECISION NOT NULL,
+    PRIMARY KEY(model_name_id)
+);
+
+
 CREATE TABLE client (
-    users_id BIGINT PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE
+    users_id BIGINT,
+    PRIMARY KEY(users_id),
+    CONSTRAINT client_fk1 FOREIGN KEY (users_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE admin (
-    users_id BIGINT PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE
+    users_id BIGINT,
+    PRIMARY KEY(users_id),
+    CONSTRAINT admin_fk1 FOREIGN KEY (users_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
--- 3. Pedidos
 CREATE TABLE request (
-    id BIGSERIAL PRIMARY KEY,
-    date_req TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    users_id BIGINT NOT NULL REFERENCES users(id)
+    id                  BIGSERIAL,
+    date_req            TIMESTAMP NOT NULL,
+    model_model_name_id BIGINT NOT NULL,
+    users_id            BIGINT NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT request_fk1 FOREIGN KEY (model_model_name_id) REFERENCES model(model_name_id),
+    CONSTRAINT request_fk2 FOREIGN KEY (users_id) REFERENCES users(id)
 );
 
--- 4. Previsões (Relacionamento 1:1 com Request)
 CREATE TABLE predictions_daily (
-    request_id BIGINT PRIMARY KEY REFERENCES request(id) ON DELETE CASCADE,
-    date_day DATE NOT NULL,
-    value_pred FLOAT8 NOT NULL
+    date_day   DATE NOT NULL,
+    value_pred DOUBLE PRECISION NOT NULL,
+    request_id BIGINT,
+    PRIMARY KEY(request_id),
+    CONSTRAINT predictions_daily_fk1 FOREIGN KEY (request_id) REFERENCES request(id) ON DELETE CASCADE
 );
 
 CREATE TABLE predictions_hourly (
-    request_id BIGINT PRIMARY KEY REFERENCES request(id) ON DELETE CASCADE,
-    date_hour TIMESTAMP NOT NULL,
-    value_pred FLOAT8 NOT NULL
+    date_hour  TIMESTAMP NOT NULL,
+    value_pred DOUBLE PRECISION NOT NULL,
+    request_id BIGINT,
+    PRIMARY KEY(request_id),
+    CONSTRAINT predictions_hourly_fk1 FOREIGN KEY (request_id) REFERENCES request(id) ON DELETE CASCADE
 );
