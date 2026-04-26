149b75c58b1237f59eb52d263366fa961a845704
franciscamateusPt05 <mateusfrancisca2005@gmail.com>
2026-04-26 15:17:50 +0100

    Path faltava

--- Code/energy_prediction_system/tests/unit/test_modeling.py
+++ Code/energy_prediction_system/tests/unit/test_modeling.py
@@ -4,6 +4,7 @@ from unittest.mock import MagicMock, patch
 import numpy as np
 import pandas as pd
 import pytest
+from pathlib import Path
 from data_pipeline.modeling import (
     DatabaseManager,
     ModelManager,
