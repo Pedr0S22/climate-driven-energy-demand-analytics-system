48f55878a1e8e64def4479064320cb7e26a29886
franciscamateusPt05 <mateusfrancisca2005@gmail.com>
2026-05-03 16:02:19 +0100

    Bash pipeline

--- Code/energy_prediction_system/src/data_pipeline/run_pipeline.sh
+++ Code/energy_prediction_system/src/data_pipeline/run_pipeline.sh
@@ -4,8 +4,14 @@ set -e
 echo "--- Iniciando Ingestion ---"
 python /src/data_pipeline/ingestion.py
 
+
 echo "--- Iniciando Cleaning ---"
 python /src/data_pipeline/cleaning.py
 
 echo "--- Iniciando Feature Engineering ---"
 python /src/data_pipeline/feature_engineering.py
+
+echo "--- Iniciando Modeling ---"
+python /src/data_pipeline/modeling.py
+
+echo "--- Pipeline concluído com sucesso! ---"
